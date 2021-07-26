b78a21e51515629091e50e5d25c63a5e8f590fe5
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-07-25 23:49:22 -0300

    Updated api methods

--- src/services/api/MealFoods/index.ts
+++ src/services/api/MealFoods/index.ts
@@ -1,63 +1,72 @@
 import { AxiosInstance, AxiosResponse } from "axios";
+import { IFood } from "../Foods";
 
 interface INutrients {
-  energy_kcal: number;
-  energy_kj: number;
-  carbs: number;
-  proteins: number;
-  fats: number;
+	energy_kcal: number;
+	energy_kj: number;
+	carbs: number;
+	proteins: number;
+	fats: number;
 }
 
-interface IMealFood extends INutrients {
-  id: string;
-  food_id: string;
-  meal_id: string;
-  name: string;
-  quantity: number;
-  quantity_unit: string;
+export interface IMealFood extends INutrients {
+	id: string;
+	food_id: string;
+	meal_id: string;
+	name: string;
+	quantity: number;
+	quantity_unit: string;
+	food: IFood;
 }
 
 export interface IAddFoodToMealRequest {
-  mealId: string;
-  foodId: string;
-  quantity: number;
-  quantity_unit: string;
+	mealId: string;
+	foodId: string;
+	quantity: number;
+	quantity_unit: string;
 }
 
 interface IUpdateMealFoodRequest {
-  quantity: number;
-  foodId: string;
+	mealFoodId: string;
+	quantity: number;
+	foodId: string;
 }
-
 export interface IDeleteMealFoodRequest {
-  mealFoodId: string;
+	mealFoodId: string;
 }
 
 const mealFoods = (axiosInstance: AxiosInstance) => {
-  return {
-    addFoodToMeal: (
-      addFoodToMealData: IAddFoodToMealRequest
-    ): Promise<AxiosResponse<IMealFood>> => {
-      return axiosInstance.post("mealfoods", {
-        ...addFoodToMealData,
-      });
-    },
+	return {
+		getById: (
+			mealFoodId: string
+		): Promise<AxiosResponse<IMealFood>> => {
+			return axiosInstance.put(`mealfoods/${mealFoodId}`);
+		},
 
-    update: (
-      updateMealFoodData: IUpdateMealFoodRequest
-    ): Promise<AxiosResponse<IMealFood>> => {
-      return axiosInstance.put("mealfoods", {
-        ...updateMealFoodData,
-      });
-    },
+		addFoodToMeal: (
+			addFoodToMealData: IAddFoodToMealRequest
+		): Promise<AxiosResponse<IMealFood>> => {
+			return axiosInstance.post("mealfoods", {
+				...addFoodToMealData,
+			});
+		},
 
-    delete: (
-      deleteMealFoodData: IDeleteMealFoodRequest
-    ): Promise<AxiosResponse<IMealFood>> => {
-      const { mealFoodId } = deleteMealFoodData;
-      return axiosInstance.delete(`mealfoods/${mealFoodId}`);
-    },
-  };
+		update: (
+			updateMealFoodData: IUpdateMealFoodRequest
+		): Promise<AxiosResponse<IMealFood>> => {
+			const { mealFoodId, ...updateData } = updateMealFoodData;
+			return axiosInstance.put(`mealfoods/${mealFoodId}`, {
+				...updateData,
+			});
+		},
+
+		delete: (
+			deleteMealFoodData: IDeleteMealFoodRequest
+		): Promise<AxiosResponse<IMealFood>> => {
+			const { mealFoodId } = deleteMealFoodData;
+			return axiosInstance.delete(`mealfoods/${mealFoodId}`);
+		},
+	};
 };
 
 export default mealFoods;
--- src/services/api/Meals/index.ts
+++ src/services/api/Meals/index.ts
@@ -1,102 +1,96 @@
 import { AxiosInstance, AxiosResponse } from "axios";
-
+import { IMealFood } from "../MealFoods";
 interface INutrients {
-  energy_kcal: number;
-  energy_kj: number;
-  carbs: number;
-  proteins: number;
-  fats: number;
+	energy_kcal: number;
+	energy_kj: number;
+	carbs: number;
+	proteins: number;
+	fats: number;
 }
-
-interface IFood extends INutrients {
-  id: string;
-  food_id: string;
-  meal_id: string;
-  name: string;
-  quantity: number;
-  quantity_unit: string;
-}
-
 export interface IMeal extends INutrients {
-  id: string;
-  name: string;
-  date: Date;
-  foods: IFood[];
+	id: string;
+	name: string;
+	date: Date;
+	mealFoods: IMealFood[];
 }
 
 interface IListByUserAndDateRequest {
-  startDate: Date;
-  endDate: Date;
+	startDate: Date;
+	endDate: Date;
 }
 
 interface IListByUserAndDateResponse extends INutrients {
-  meals: IMeal[];
+	meals: IMeal[];
 }
 
 export interface IAddMealRequest {
-  name: string;
-  date: Date;
+	name: string;
+	date: Date;
 }
 
 interface IAddMealResponse {
-  id: string;
-  user_id: string;
-  name: string;
-  date: Date;
+	id: string;
+	user_id: string;
+	name: string;
+	date: Date;
 }
 
 export interface IDeleteMealRequest {
-  mealId: string;
+	mealId: string;
 }
 
 export interface IUpdateMealNameRequest {
-  mealId: string;
-  name: string;
+	mealId: string;
+	name: string;
 }
 
 interface IUpdateMealNameResponse {
-  id: string;
-  user_id: string;
-  name: string;
-  date: Date;
+	id: string;
+	user_id: string;
+	name: string;
+	date: Date;
 }
 
 const meals = (axiosInstance: AxiosInstance) => {
-  return {
-    listByUserAndDate: (
-      listMealsData: IListByUserAndDateRequest
-    ): Promise<AxiosResponse<IListByUserAndDateResponse>> => {
-      return axiosInstance.get("meals", {
-        params: {
-          ...listMealsData,
-        },
-      });
-    },
+	return {
+		listByUserAndDate: (
+			listMealsData: IListByUserAndDateRequest
+		): Promise<AxiosResponse<IListByUserAndDateResponse>> => {
+			return axiosInstance.get("meals", {
+				params: {
+					...listMealsData,
+				},
+			});
+		},
 
-    add: (
-      addMealData: IAddMealRequest
-    ): Promise<AxiosResponse<IAddMealResponse>> => {
-      return axiosInstance.post<IAddMealResponse>("meals", {
-        ...addMealData,
-      });
-    },
+		getById: (mealId: string): Promise<AxiosResponse<IMeal>> => {
+			return axiosInstance.get(`meals/${mealId}`);
+		},
 
-    update: (
-      updateMealNameData: IUpdateMealNameRequest
-    ): Promise<AxiosResponse<IUpdateMealNameResponse>> => {
-      const { mealId } = updateMealNameData;
+		add: (
+			addMealData: IAddMealRequest
+		): Promise<AxiosResponse<IAddMealResponse>> => {
+			return axiosInstance.post<IAddMealResponse>("meals", {
+				...addMealData,
+			});
+		},
 
-      return axiosInstance.put(`meals/${mealId}`);
-    },
+		update: (
+			updateMealNameData: IUpdateMealNameRequest
+		): Promise<AxiosResponse<IUpdateMealNameResponse>> => {
+			const { mealId } = updateMealNameData;
 
-    delete: (
-      deleteMealData: IDeleteMealRequest
-    ): Promise<AxiosResponse<IMeal>> => {
-      const { mealId } = deleteMealData;
+			return axiosInstance.put(`meals/${mealId}`);
+		},
 
-      return axiosInstance.delete(`meals/${mealId}`);
-    },
-  };
+		delete: (
+			deleteMealData: IDeleteMealRequest
+		): Promise<AxiosResponse<IMeal>> => {
+			const { mealId } = deleteMealData;
+
+			return axiosInstance.delete(`meals/${mealId}`);
+		},
+	};
 };
 
 export default meals;
