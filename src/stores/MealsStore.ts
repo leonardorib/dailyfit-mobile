ce0d2fa82fdbf81a163c671e5a099a126d08d405
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-07-25 23:52:29 -0300

    Updated global meal store types and formatting

--- src/stores/MealsStore.ts
+++ src/stores/MealsStore.ts
@@ -2,243 +2,230 @@ import { createContext } from "react";
 import { action, makeObservable, observable, runInAction } from "mobx";
 import api from "../services/api";
 import {
-  IMeal,
-  IUpdateMealNameRequest,
-  IDeleteMealRequest,
+	IMeal,
+	IUpdateMealNameRequest,
+	IDeleteMealRequest,
 } from "../services/api/Meals";
 import {
-  IAddFoodToMealRequest,
-  IDeleteMealFoodRequest,
+	IAddFoodToMealRequest,
+	IDeleteMealFoodRequest,
 } from "../services/api/MealFoods";
 import { addDays, endOfDay, startOfDay, subDays } from "date-fns";
 import { Alert } from "react-native";
 
 export interface INutrients {
-  energy_kcal: number;
-  energy_kj: number;
-  carbs: number;
-  proteins: number;
-  fats: number;
-}
-
-export interface IMealFood extends INutrients {
-  id: string;
-  food_id: string;
-  name: string;
-  quantity: number;
-  quantity_unit: string;
+	energy_kcal: number;
+	energy_kj: number;
+	carbs: number;
+	proteins: number;
+	fats: number;
 }
 
 export interface IDailyDiet extends INutrients {
-  meals: IMeal[];
+	meals: IMeal[];
 }
 
 export class MealsStore {
-  constructor() {
-    makeObservable(this);
-  }
+	constructor() {
+		makeObservable(this);
+	}
 
-  @observable selectedDate: Date = new Date(Date.now());
+	@observable selectedDate: Date = new Date(Date.now());
 
-  @observable dailyDiet: IDailyDiet = {
-    energy_kcal: 0,
-    energy_kj: 0,
-    carbs: 0,
-    proteins: 0,
-    fats: 0,
-    meals: [],
-  };
+	@observable dailyDiet: IDailyDiet = {
+		energy_kcal: 0,
+		energy_kj: 0,
+		carbs: 0,
+		proteins: 0,
+		fats: 0,
+		meals: [],
+	};
 
-  @observable isAddMealModalVisible: boolean = false;
+	@observable isAddMealModalVisible: boolean = false;
 
-  @observable isAddFoodModalVisible: boolean = false;
+	@observable isAddFoodModalVisible: boolean = false;
 
-  @action setSelectedDate = (date: Date) => {
-    this.selectedDate = date;
-  };
+	@action setSelectedDate = (date: Date) => {
+		this.selectedDate = date;
+	};
 
-  @action subtractDay = () => {
-    this.selectedDate = subDays(this.selectedDate, 1);
-  };
+	@action subtractDay = () => {
+		this.selectedDate = subDays(this.selectedDate, 1);
+	};
 
-  @action addDay = () => {
-    this.selectedDate = addDays(this.selectedDate, 1);
-  };
+	@action addDay = () => {
+		this.selectedDate = addDays(this.selectedDate, 1);
+	};
 
-  // Meals
-  getDailyDiet = async () => {
-    try {
-      const response = await api.meals.listByUserAndDate({
-        startDate: startOfDay(this.selectedDate),
-        endDate: endOfDay(this.selectedDate),
-      });
+	// Meals
+	getDailyDiet = async () => {
+		try {
+			const response = await api.meals.listByUserAndDate({
+				startDate: startOfDay(this.selectedDate),
+				endDate: endOfDay(this.selectedDate),
+			});
 
-      runInAction(() => {
-        this.dailyDiet = response.data;
-      });
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+			runInAction(() => {
+				this.dailyDiet = response.data;
+			});
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 
-  createMeal = async (mealName: string) => {
-    try {
-      const response = await api.meals.add({
-        name: mealName,
-        date: this.selectedDate,
-      });
+	createMeal = async (mealName: string) => {
+		try {
+			const response = await api.meals.add({
+				name: mealName,
+				date: this.selectedDate,
+			});
 
-      runInAction(() => {
-        this.dailyDiet.meals.push({
-          ...response.data,
-          energy_kcal: 0,
-          energy_kj: 0,
-          carbs: 0,
-          proteins: 0,
-          fats: 0,
-          foods: [],
-        });
-      });
-    } catch (error) {
-      Alert.alert("Erro ao criar refeição", "Tente novamente");
-      console.error(error.message);
-    }
-  };
+			runInAction(() => {
+				this.dailyDiet.meals.push({
+					...response.data,
+					energy_kcal: 0,
+					energy_kj: 0,
+					carbs: 0,
+					proteins: 0,
+					fats: 0,
+					mealFoods: [],
+				});
+			});
+		} catch (error) {
+			Alert.alert("Erro ao criar refeição", "Tente novamente");
+			console.error(error.message);
+		}
+	};
 
-  updateMealName = async ({ mealId, name }: IUpdateMealNameRequest) => {
-    try {
-      await api.meals.update({ mealId, name });
+	updateMealName = async ({ mealId, name }: IUpdateMealNameRequest) => {
+		try {
+			await api.meals.update({ mealId, name });
 
-      const currentMeals = this.dailyDiet.meals;
+			const currentMeals = this.dailyDiet.meals;
 
-      const updatedMeals = currentMeals.map((meal) => {
-        if (meal.id === mealId) {
-          return {
-            ...meal,
-            name,
-          };
-        } else {
-          return meal;
-        }
-      });
+			const updatedMeals = currentMeals.map((meal) => {
+				if (meal.id === mealId) {
+					return {
+						...meal,
+						name,
+					};
+				} else {
+					return meal;
+				}
+			});
 
-      runInAction(() => {
-        this.dailyDiet.meals = updatedMeals;
-      });
-    } catch (error) {
-      Alert.alert("Erro ao atualizar refeição", "Tente novamente");
-      console.error(error.message);
-    }
-  };
+			runInAction(() => {
+				this.dailyDiet.meals = updatedMeals;
+			});
+		} catch (error) {
+			Alert.alert("Erro ao atualizar refeição", "Tente novamente");
+			console.error(error.message);
+		}
+	};
 
-  deleteMeal = async ({ mealId }: IDeleteMealRequest) => {
-    try {
-      const response = await api.meals.delete({ mealId });
+	deleteMeal = async ({ mealId }: IDeleteMealRequest) => {
+		try {
+			const response = await api.meals.delete({ mealId });
 
-      const { energy_kcal, energy_kj, carbs, proteins, fats } = response.data;
+			const { energy_kcal, energy_kj, carbs, proteins, fats } =
+				response.data;
 
-      runInAction(() => {
-        const updatedMeals = this.dailyDiet.meals.filter(
-          (meal) => meal.id !== mealId
-        );
-        this.dailyDiet.meals = updatedMeals;
-        this.dailyDiet.energy_kcal -= energy_kcal;
-        this.dailyDiet.energy_kj -= energy_kj;
-        this.dailyDiet.carbs -= carbs;
-        this.dailyDiet.proteins -= proteins;
-        this.dailyDiet.fats -= fats;
-      });
-    } catch (error) {
-      Alert.alert("Erro ao deletar refeição", "Tente novamente");
-      console.error(error.message);
-    }
-  };
+			runInAction(() => {
+				const updatedMeals = this.dailyDiet.meals.filter(
+					(meal) => meal.id !== mealId
+				);
+				this.dailyDiet.meals = updatedMeals;
+				this.dailyDiet.energy_kcal -= energy_kcal;
+				this.dailyDiet.energy_kj -= energy_kj;
+				this.dailyDiet.carbs -= carbs;
+				this.dailyDiet.proteins -= proteins;
+				this.dailyDiet.fats -= fats;
+			});
+		} catch (error) {
+			Alert.alert("Erro ao deletar refeição", "Tente novamente");
+			console.error(error.message);
+		}
+	};
 
-  // MealFoods
-  addFoodToMeal = async ({
-    mealId,
-    foodId,
-    quantity,
-    quantity_unit,
-  }: IAddFoodToMealRequest) => {
-    try {
-      const response = await api.mealFoods.addFoodToMeal({
-        mealId,
-        foodId,
-        quantity,
-        quantity_unit,
-      });
-      const food = response.data;
-      const { energy_kcal, energy_kj, carbs, proteins, fats } = food;
+	// MealFoods
+	addFoodToMeal = async ({
+		mealId,
+		foodId,
+		quantity,
+		quantity_unit,
+	}: IAddFoodToMealRequest) => {
+		try {
+			const response = await api.mealFoods.addFoodToMeal({
+				mealId,
+				foodId,
+				quantity,
+				quantity_unit,
+			});
+			const food = response.data;
+			const { energy_kcal, energy_kj, carbs, proteins, fats } = food;
 
-      runInAction(() => {
-        this.dailyDiet.energy_kcal += energy_kcal;
-        this.dailyDiet.energy_kj += energy_kj;
-        this.dailyDiet.carbs += carbs;
-        this.dailyDiet.proteins += proteins;
-        this.dailyDiet.fats += fats;
+			runInAction(() => {
+				this.dailyDiet.energy_kcal += energy_kcal;
+				this.dailyDiet.energy_kj += energy_kj;
+				this.dailyDiet.carbs += carbs;
+				this.dailyDiet.proteins += proteins;
+				this.dailyDiet.fats += fats;
 
-        const updatedMeals = this.dailyDiet.meals.map((meal) => {
-          if (meal.id === mealId) {
-            meal.foods.push(food);
-            return {
-              ...meal,
-              energy_kcal: meal.energy_kcal + energy_kcal,
-              energy_kj: meal.energy_kj + energy_kj,
-              carbs: meal.carbs + carbs,
-              proteins: meal.proteins + proteins,
-              fats: meal.fats + fats,
-            };
-          } else {
-            return meal;
-          }
-        });
-        this.dailyDiet.meals = updatedMeals;
-      });
+				const updatedMeals = this.dailyDiet.meals.map((meal) => {
+					if (meal.id === mealId) {
+						meal.mealFoods.push(food);
+						return {
+							...meal,
+							energy_kcal: meal.energy_kcal + energy_kcal,
+							energy_kj: meal.energy_kj + energy_kj,
+							carbs: meal.carbs + carbs,
+							proteins: meal.proteins + proteins,
+							fats: meal.fats + fats,
+						};
+					} else {
+						return meal;
+					}
+				});
+				this.dailyDiet.meals = updatedMeals;
+			});
 
-      return response.data;
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+			return response.data;
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 
-  deleteMealFood = async ({ mealFoodId }: IDeleteMealFoodRequest) => {
-    try {
-      const response = await api.mealFoods.delete({ mealFoodId });
+	deleteMealFood = async ({ mealFoodId }: IDeleteMealFoodRequest) => {
+		try {
+			const response = await api.mealFoods.delete({ mealFoodId });
 
-      const {
-        energy_kcal,
-        energy_kj,
-        carbs,
-        proteins,
-        fats,
-        meal_id,
-      } = response.data;
+			const { energy_kcal, energy_kj, carbs, proteins, fats, meal_id } =
+				response.data;
 
-      runInAction(() => {
-        this.dailyDiet.energy_kcal -= energy_kcal;
-        this.dailyDiet.energy_kj -= energy_kj;
-        this.dailyDiet.carbs -= carbs;
-        this.dailyDiet.proteins -= proteins;
-        this.dailyDiet.fats -= fats;
+			runInAction(() => {
+				this.dailyDiet.energy_kcal -= energy_kcal;
+				this.dailyDiet.energy_kj -= energy_kj;
+				this.dailyDiet.carbs -= carbs;
+				this.dailyDiet.proteins -= proteins;
+				this.dailyDiet.fats -= fats;
 
-        this.dailyDiet.meals.map((meal) => {
-          if (meal.id === meal_id) {
-            return {
-              ...meal,
-              energy_kcal: meal.energy_kcal - energy_kcal,
-              energy_kj: meal.energy_kj - energy_kj,
-              carbs: meal.carbs - carbs,
-              proteins: meal.proteins - proteins,
-              fats: meal.fats - fats,
-            };
-          }
-        });
-      });
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+				this.dailyDiet.meals.map((meal) => {
+					if (meal.id === meal_id) {
+						return {
+							...meal,
+							energy_kcal: meal.energy_kcal - energy_kcal,
+							energy_kj: meal.energy_kj - energy_kj,
+							carbs: meal.carbs - carbs,
+							proteins: meal.proteins - proteins,
+							fats: meal.fats - fats,
+						};
+					}
+				});
+			});
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 }
 
 export const MealsStoreContext = createContext(new MealsStore());
