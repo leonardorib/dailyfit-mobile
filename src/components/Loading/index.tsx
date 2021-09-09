aeafb24b7d4add62d202d39b998b2a792e28e3b9
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-09-08 21:18:24 -0300

    Style adjustment on Loading and containerStyle prop

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, StyleProp, ViewStyle } from "react-native";
 import { Container } from "./styles";
 
-export const Loading: React.FC = () => {
+interface IProps {
+	containerStyle?: StyleProp<ViewStyle>
+}
+
+export const Loading: React.FC<IProps> = (props) => {
+	const { containerStyle } = props;
 	return (
-		<Container>
-			<ActivityIndicator />
+		<Container style={containerStyle} >
+			<ActivityIndicator color="#76c7c5" />
 		</Container>
 	);
 };
