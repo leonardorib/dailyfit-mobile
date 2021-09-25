a53228a2b3a3110af67ba4acd63de9ca642d4803
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-09-25 18:10:31 -0300

    Added possibility to set duration to toast

--- src/services/flashMessage/index.ts
+++ src/services/flashMessage/index.ts
@@ -1,17 +1,19 @@
 import { showMessage } from "react-native-flash-message";
 
-export const showError = (message: string) => {
+export const showError = (message: string, duration = 3000) => {
 	showMessage({
 		position: "top",
 		message: message,
 		type: "danger",
+		duration,
 	});
 }
 
-export const showSuccess = (message: string) => {
+export const showSuccess = (message: string, duration = 3000) => {
 	showMessage({
 		position: "top",
 		message: message,
 		type: "success",
+		duration,
 	});
 }
