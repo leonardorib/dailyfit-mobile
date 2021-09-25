ca6a1e8d01f217b95f53c7fb2f726216ced5793f
Leonardo Ribeiro <leonardo.rib@hotmail.com>
2021-09-25 18:08:04 -0300

    Added white variant to Loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,14 +3,15 @@ import { ActivityIndicator, StyleProp, ViewStyle } from "react-native";
 import { Container } from "./styles";
 
 interface IProps {
-	containerStyle?: StyleProp<ViewStyle>
+	variant?: "white" | "green";
+	containerStyle?: StyleProp<ViewStyle>;
 }
 
 export const Loading: React.FC<IProps> = (props) => {
-	const { containerStyle } = props;
+	const { variant, containerStyle } = props;
 	return (
 		<Container style={containerStyle} >
-			<ActivityIndicator color="#76c7c5" />
+			<ActivityIndicator color={variant === "white" ? "#fff" : "#76c7c5"} />
 		</Container>
 	);
 };
